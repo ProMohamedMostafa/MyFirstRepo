901dfe1ec9d56c5236e02ecbc4045ec00e4f79b6
pro.mohamedmostafa <pro.mohamedmostafa@gmail.com>
2019-11-12 03:38:54 +0200

    create first Controller

---
+++ MyFirstAppWithGitHub2/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstAppWithGitHub2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public string Index()
+        {
+            return "Mohamed Mostafa Is the Best programmer i the world";
+        }
+    }
+}
